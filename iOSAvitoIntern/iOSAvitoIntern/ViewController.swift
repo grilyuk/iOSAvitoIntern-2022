c3ffcb8191eeecb3b44230fd92e596f15013d494
grilyuk <danilyuk1999@inbox.ru>
2022-10-23 22:08:44 +0300

    start

---
+++ iOSAvitoIntern/iOSAvitoIntern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAvitoIntern
+//
+//  Created by Григорий Данилюк on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
